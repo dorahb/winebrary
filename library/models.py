11c64599bad789e5a4d52c7671e5d450cd623b40
Nyambura🌻 <bianca@Nyambura-C02FP39KQ6LC-SW.local>
2022-11-03 22:12:36 +0300

    update initial migrations

--- library/models.py
+++ library/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+  image = models.ImageField(null=True, blank=True)  
+  title = models.CharField(max_length=200)
+  author = models.CharField(max_length=100)
+  synopsis = models.CharField(max_length=1000)
+  pub_date = models.DateTimeField('date posted')
