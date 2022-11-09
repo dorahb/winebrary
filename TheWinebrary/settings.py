406d8e44778a6d788853788639d08ca021d0a989
Nyambura🌻 <bianca@Nyambura-C02FP39KQ6LC-SW.local>
2022-11-09 21:13:25 +0300

    configure media path in settings

--- TheWinebrary/settings.py
+++ TheWinebrary/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'library.apps.LibraryConfig'
+    'library.apps.LibraryConfig',
+    'cloudinary',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +76,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'TheWinebrary.wsgi.application'
 
+cloudinary.config( 
+  cloud_name = "dry0oqcgq", 
+  api_key = "942199376718611", 
+  api_secret = "hc0nKD19l6hHkM82IWWDsYxV9oY" 
+)
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -116,9 +127,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# configuring the location for media
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
