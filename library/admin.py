877c47d0ae3a2a00eebdeabca376c443df5b35f9
Nyambura🌻 <bianca@Nyambura-C02FP39KQ6LC-SW.local>
2022-11-16 21:59:07 +0300

    update models

--- library/admin.py
+++ library/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Book, Profile
+from .models import *
 
 admin.site.register(Profile)
 admin.site.register(Book)
+admin.site.register(Swap)
+admin.site.register(Wishlist)
+admin.site.register(ShippingAddress)
--- library/models.py
+++ library/models.py
@@ -4,16 +4,17 @@ from django.contrib.auth.models import User
 # Create your models here.  
 
 class Profile(models.Model):
-  user = models.ForeignKey(User, on_delete = models.CASCADE)
+  user = models.OneToOneField(User, on_delete = models.CASCADE)
+  name =  models.CharField(max_length=200, null=True)
   profilepic = models.ImageField(null=True, blank=True) 
   email =  models.CharField(max_length=100)
   bio = models.CharField(max_length=500)
  
   def __str__(self):
-        return self.user 
+        return self.name 
 
 class Book(models.Model):
-  user = models.ForeignKey(User,on_delete = models.CASCADE)
+  user = models.ForeignKey(Profile,on_delete = models.CASCADE)
   image = models.ImageField(null=True, blank=True) 
   title = models.CharField(max_length=200)
   author = models.CharField(max_length=100)
@@ -32,3 +33,31 @@ class Book(models.Model):
       url = ''
     return url
   
+
+class Swap (models.Model):
+  user = models.ForeignKey(Profile, on_delete=models.SET_NULL,null=True, blank=True)
+  book = models. ForeignKey(Book, on_delete=models.SET_NULL,null=True, blank=True)
+  date_swapped = models.DateTimeField(auto_now_add=True)
+  complete = models.BooleanField(default=False)
+  swap_id = models.CharField(max_length=100, null=True) 
+ 
+
+  def __str__(self):
+        return str(self.id)
+
+class Wishlist (models.Model):
+  user = models.ForeignKey(Profile, on_delete=models.SET_NULL,null=True, blank=True)
+  book = models. ForeignKey(Book, on_delete=models.SET_NULL,null=True, blank=True)
+
+  def __str__(self):
+        return str(self.book)
+
+class ShippingAddress (models.Model):
+  user = models. ForeignKey(Profile, on_delete=models.SET_NULL,null=True, blank=True)
+  swap = models. ForeignKey(Swap, on_delete=models.SET_NULL,null=True, blank=True)
+  address = models.CharField(max_length=200,null=False)
+  date = models.DateTimeField(auto_now_add=True)
+ 
+
+  def __str__(self):
+        return str(self.address)
